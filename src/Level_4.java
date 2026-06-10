932a0e7babff860047bb26571fa136706e6fbe50
tiendungcln <tiendung0301cln@gmail.com>
2026-06-10 15:36:29 +0700

    add Level_4

---
+++ src/Level_4.java
@@ -0,0 +1,38 @@
+public class Level_4 {
+    // Cho: arr = [1,2,3,4,5]
+    // target = 6
+    // Kết quả:
+    // (1,5)
+    // (2,4)
+
+    public static void findPairsSum(int[] arr, int target){
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right){
+
+            if (arr[left] + arr[right] > target){
+                right--;
+            } else if (arr[left] + arr[right] < target) {
+                left++;
+            } else {
+                System.out.println(arr[left] + " - " + arr[right]);
+                right--;
+                left++;
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        int[] arr = {1, 2, 3, 4, 6};
+        int target = 6;
+
+        findPairsSum(arr, target);
+
+    }
+
+}
